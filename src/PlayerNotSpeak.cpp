26a8ff57156071b58aae7bcd8f64aa282e4db4fa
Vitaliy <mitradis@gmail.com>
2021-03-02 13:29:14 +0300

    Update PlayerNotSpeak.cpp

--- src/PlayerNotSpeak.cpp
+++ src/PlayerNotSpeak.cpp
@@ -33,32 +33,7 @@ public:
 	}
 };
 
-class KargatumSC_PlayerNotSpeak_World : public WorldScript
-{
-public:
-    KargatumSC_PlayerNotSpeak_World() : WorldScript("KargatumSC_PlayerNotSpeak_World") {}
-
-    void OnAfterConfigLoad(bool /*reload*/) override
-    {
-        std::string conf_path = _CONF_DIR;
-        std::string cfg_file = conf_path + "/modules/PlayerNotSpeak.conf";
-
-#if PLATFORM == PLATFORM_WINDOWS
-        cfg_file = "modules/PlayerNotSpeak.conf";
-#endif
-        if (!sConfigMgr->LoadMore(cfg_file.c_str()))
-		{
-			sLog->outString();
-			sLog->outError("Config: Invalid or missing configuration file : %s", cfg_file.c_str());
-			sLog->outError("Config: Verify that the file exists and has \'[worldserver]' written in the top of the file!");
-			sLog->outError("Config: Use default settings!");
-			sLog->outString();
-		}
-    }
-};
-
 void AddPlayerNotSpeakScripts()
 {
 	new KargatumSC_PlayerNotSpeak();
-    new KargatumSC_PlayerNotSpeak_World();
 }
