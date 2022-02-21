11d8ac3b8d87ac97832db9bb3b132527e1db821c
Yuen Kwong <31172380+adink1@users.noreply.github.com>
2022-02-21 11:41:07 +0800

    Update trinity_string.sql
    fix database format

--- sql/world/trinity_string.sql
+++ sql/world/trinity_string.sql
@@ -1,4 +1,4 @@
 -- Language
 
-INSERT INTO `trinity_string`(`entry`, `content_default`, `content_loc8`) VALUES
+INSERT INTO `acore_string`(`entry`, `content_default`, `locale_ruRU`) VALUES
 (13000, 'Player %s will not be able to speak yet: %s', '|cffff0000# |cff00ff00Игрок|r %s |cff00ff00не сможет говорить ещё: |cffff0000%s');
